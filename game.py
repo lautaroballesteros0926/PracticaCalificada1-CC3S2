e17713b60952b12e7322687e25397e8423a471dd
CesarSanchez04 <cesar.asm2004@gmail.com>
2024-09-19 22:28:49 -0500

    agregando modo 2 jugadores

--- game.py
+++ game.py
@@ -8,7 +8,8 @@ class Game:
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
         self.background = Background()
-        self.player = Player()
+        self.player1 = Player()
+        self.player2 = Player()
         self.meteorites = pygame.sprite.Group()
 
     def loop(self):
@@ -27,16 +28,22 @@ class Game:
     def handle_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.player.move(-5, 0)
+            self.player1.move(-5, 0)
         if keys[pygame.K_RIGHT]:
-            self.player.move(5, 0)
+            self.player1.move(5, 0)
+        if keys[pygame.K_a]:
+            self.player2.move(-5, 0)
+        if keys[pygame.K_d]:
+            self.player2.move(5, 0)
 
     def update(self):
         self.meteorites.update()
-        self.player.update()
+        self.player1.update()
+        self.player2.update()
 
     def draw(self):
         self.screen.blit(self.background.image, self.background.rect)
-        self.screen.blit(self.player.image, self.player.rect)
+        self.screen.blit(self.player1.image, self.player1.rect)
+        self.screen.blit(self.player2.image, self.player2.rect)
         self.meteorites.draw(self.screen)
         pygame.display.flip()
